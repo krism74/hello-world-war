f9570ce924921d9ef45c6c9b169d7ffd8d305888
krism74 <krism74@users.noreply.github.com>
2015-02-03 21:34:56 -0600

    Create push2Docker.sh

---
+++ push2Docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOCKER_TAG_BASE="helloworld_"
+DOCKER_USERNAME=$1
+DOCKER_PASSWORD=$2
+DOCKER_EMAIL=$3
+DOCKER_IP=$4
+BUILD_NUMBER=$5
+
+sudo docker build -t "helloworld_"$BUILD_NUMBER .
+sudo docker tag "helloworld_"$BUILD_NUMBER 96.239.251.10:9000/$DOCKER_USERNAME/"helloworld_"$BUILD_NUMBER
+sudo docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD -e $DOCKER_EMAIL $DOCKER_IP
+sudo docker push $DOCKER_IP/$DOCKER_USERNAME/"helloworld_"$BUILD_NUMBER
